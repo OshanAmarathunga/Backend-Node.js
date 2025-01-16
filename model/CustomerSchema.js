7f1835bb36dca802413e18d860dd7ac9f1367c14
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-16 13:20:30 +0530

    first commit

---
+++ model/CustomerSchema.js
@@ -0,0 +1,29 @@
+const mongoose=require('mongoose');
+const CustomerSchema=new mongoose.Schema({
+    customerName:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    salary:{
+        type:Number,
+        required:true
+    },
+    contact:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    isActive:{
+        type:Boolean,
+        required:true
+    }
+});
+
+module.exports=mongoose.model('Customer',CustomerSchema);
