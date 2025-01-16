81e9a15ff63e3940e67524b6c7c8a7361fb0753e
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-16 20:13:18 +0530

    added saveOrder function

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,1 +1,18 @@
 import Order from '../model/OrderSchema.js'
+
+export async function saveOrder(req,res){ //admin, manager
+    try {
+        const newOrder=new Order(req.body);
+        const savedOrder=await newOrder.save();
+
+        res.status(200).json({
+            message:"Order save success!",
+            data:savedOrder
+        });
+        
+    } catch (e) {
+        res.status(500).json({
+            errorMessage:e.message
+        })
+    }
+}
--- route/OrderRouter.js
+++ route/OrderRouter.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { saveOrder } from '../controller/OrderController.js';
 
 const router=express.Router();
 
+router.post('/saveOrder',saveOrder);
 
 
 export default router;
