5fef015f1c00c79ed7f91dd2709e65f109484c2b
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-16 23:31:47 +0530

    findIncomeMonth

--- controller/PaymentController.js
+++ controller/PaymentController.js
@@ -29,12 +29,43 @@ export async function findIncomeToday(req, res) {
       },
     });
 
-    const totalIncome = data.reduce((sum, payment) => {
-      sum + payment.amount, 0;
-    });
+    const totalIncome = data.reduce((sum, payment) => sum + payment.amount, 0);
     res.status(200).json({
       message: "today's income",
       data: totalIncome,
+    });
+  } catch (e) {
+    res.status(500).json({
+      errorMessage: e.message,
+    });
+  }
+}
+
+export async function findIncomeMonth(req, res) {
+  try {
+    //   const day = req.query; //yyyy-mm-dd
+    const now = new Date();
+    const startOfMonth = new Date(now.getFullYear, now.getMonth(), 1);
+    const endOfMonth = new Date(now.getFullYear, now.getMonth() + 1, 1);
+
+    const data = await Payment.find({
+      date: {
+        $gtq: startOfMonth,
+        $lt: endOfMonth,
+      },
+    });
+
+    const incomeByDay = data.reduce((sum, payment) => {
+      const day = payment.date.toISOString().split("T")[0];
+      sum[day] = (sum[day] || 0) + payment.amount;
+      return sum;
+    }, {});
+    res.status(200).json({
+      message: "month's income",
+      data: {
+        month: now.getMonth() + 1,
+        income: incomeByDay,
+      },
     });
   } catch (e) {
     res.status(500).json({
