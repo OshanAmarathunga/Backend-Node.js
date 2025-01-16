c0a80261376084f6f0d983d7e0a3067a846ed895
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-16 19:55:22 +0530

    added all the fucntions

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -15,7 +15,7 @@ export async function saveCustomer(req,res){ //admin / manager
     }
 }
 
-export async function updateCustomer(req,res) {// admin , manager
+export async function updateCustomer(req,res) {// admin 
     try {
         const updatedCustomer =await Customer.findByIdAndUpdate(
             req.params.id,
@@ -25,7 +25,7 @@ export async function updateCustomer(req,res) {// admin , manager
             }
         );
 
-        if(updateCustomer){
+        if(updatedCustomer){
           return  res.status(201).json({
                 message:"Update success",
                 data:updatedCustomer
@@ -38,6 +38,73 @@ export async function updateCustomer(req,res) {// admin , manager
     } catch (e) {
         res.status(500).json({
             updateError:e.message
+        })
+    }
+}
+
+export async function deleteCustomer(req,res){ //admin
+    try {
+        const deletedCustomer =await Customer.findByIdAndDelete({_id:req.body._id});
+        if(deleteCustomer){
+            return res.status(204).json({
+                message:`Customer deleted`,
+                data:deletedCustomer
+            })
+        }
+
+        res.status(404).json({
+            message:"Customer not Found!"
+        })
+    } catch (e) {
+        res.status(500).json({
+            deleteError:e.message
+        })
+    }
+}
+
+export async function getCustomerById(req,res){ // all
+    const customerId=req.params.id;
+    try {
+       const getCustomer= await Customer.findOne({_id:customerId});
+       if(getCustomer){
+        return res.status(200).json({
+            message : "Customer found",
+            data:getCustomer
+        })
+       }
+       return res.status(404).json({
+        message : "Customer not Found",
+        
+    })
+       
+    } catch (e) {
+        res.status(500).json({
+            errorMassage:e.message
+        })
+    }
+}
+
+export async function getAllCustomers(req,res){ //all
+    try {
+        const {searchText, page=1, size=10} =req.query;
+        const filter=searchText?{$or:[
+            {customerName:{$regex:searchText, $options:"i"}},
+            {address:{$regex:searchText, $options:"i"}},
+            {email:{$regex:searchText, $options:"i"}}
+        ]}:{};
+
+        const customerList=await Customer.find(filter).skip((page-1)*size).limit(parseInt(size));
+        const total=await Customer.countDocuments(filter);
+
+        res.status(200).json({
+            message:"Data List",
+            dataList:customerList,
+            count:total
+        })
+
+    } catch (e) {
+        res.status(500).json({
+            errorMessage:e.message
         })
     }
 }
--- route/CustomerRoute.js
+++ route/CustomerRoute.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { saveCustomer, updateCustomer } from '../controller/CustomerController.js';
+import { deleteCustomer, getAllCustomers, getCustomerById, saveCustomer, updateCustomer } from '../controller/CustomerController.js';
 
 const router=express.Router();
 
 router.post('/saveCustomer', saveCustomer);
 router.put('/updateCustomer',updateCustomer);
+router.delete('/deleteCustomer', deleteCustomer);
+router.get('/getCustomerById:id',getCustomerById);
+router.get('getAllCustomers', getAllCustomers);
 
 
 export default router;
