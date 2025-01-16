8c8ca8c1818caf820ff80a8cbb56a65f3dbc3be6
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-16 23:12:32 +0530

    findIncomeToday

--- controller/PaymentController.js
+++ controller/PaymentController.js
@@ -1,17 +1,44 @@
+import Payment from "../model/PaymentSchema.js";
 
-import Payment from '../model/PaymentSchema.js'
+export async function makePayment(req, res) {
+  try {
+    const newPayment = new Payment(req.body);
+    const savedPayment = await newPayment.save();
+    res.status(200).json({
+      message: "Payment made success!",
+      date: savedPayment,
+    });
+  } catch (e) {
+    res.status(500).json({
+      errorMessage: e.message,
+    });
+  }
+}
 
-export async function makePayment(req,res){
-    try {
-        const newPayment=new Payment(req.body);
-        const savedPayment=await newPayment.save();
-        res.status(200).json({
-            message:"Payment made success!",
-            date:savedPayment
-        })
-    } catch (e) {
-        res.status(500).json({
-            errorMessage:e.message
-        })
-    }
+export async function findIncomeToday(req, res) {
+  try {
+    const day = req.query; //yyyy-mm-dd
+    const startDay = new Date(day);
+    const endDay = new Date(day);
+
+    endDay.setDate(endDay.getDate() + 1);
+    const data = await Payment.find({
+      date: {
+        $gtq: startDay,
+        $lt: endDay,
+      },
+    });
+
+    const totalIncome = data.reduce((sum, payment) => {
+      sum + payment.amount, 0;
+    });
+    res.status(200).json({
+      message: "today's income",
+      data: totalIncome,
+    });
+  } catch (e) {
+    res.status(500).json({
+      errorMessage: e.message,
+    });
+  }
 }
--- route/Paymentroute.js
+++ route/Paymentroute.js
@@ -6,4 +6,5 @@ const router =express.Router();
 router.post('savePayment',makePayment);
 
 
+
 export default router;
