ffaad216f2882c1667c41a69f17170891b1e17ad
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-17 13:58:04 +0530

    added deleteProduct

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -1,43 +1,61 @@
-import Product from '../model/ProductSchema.js'
+import Product from "../model/ProductSchema.js";
 
-export async function saveProduct(req,res){
-    try {
-        const newProduct=new Product(req.body);
-        const savedProduct=await newProduct.save();
+export async function saveProduct(req, res) {
+  try {
+    const newProduct = new Product(req.body);
+    const savedProduct = await newProduct.save();
 
-        res.status(200).json({
-            messsage:"Product save success!",
-            data:savedProduct
-        })
-    } catch (e) {
-        res.status(500).json({
-            errorMessage:e.message
-        })
-    }
+    res.status(200).json({
+      messsage: "Product save success!",
+      data: savedProduct,
+    });
+  } catch (e) {
+    res.status(500).json({
+      errorMessage: e.message,
+    });
+  }
 }
 
-export async function updateProduct(req,res){
-    try {
-       const updatedProduct= await Product.findByIdAndUpdate(
-            req.params.id,
-            req.body
-        )
+export async function updateProduct(req, res) {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
 
-        if(updatedProduct){
-            return res.status(200).json({
-                message:"Product updated!",
-                data:updatedProduct
-            })
-        }
-
-        res.status(404).json({
-            message:"Product not found!"
-        })
-
-
-    } catch (e) {
-        res.status(500).json({
-            errorMessage:e.message
-        })
+    if (updatedProduct) {
+      return res.status(200).json({
+        message: "Product updated!",
+        data: updatedProduct,
+      });
     }
+
+    res.status(404).json({
+      message: "Product not found!",
+    });
+  } catch (e) {
+    res.status(500).json({
+      errorMessage: e.message,
+    });
+  }
+}
+
+export async function deleteProduct(req, res) {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (deletedProduct) {
+      return res.status(200).json({
+        message: "Product delete success!",
+        data: deletedProduct,
+      });
+    }
+
+    res.status(404).json({
+      message: "Product not found!",
+    });
+  } catch (e) {
+    res.status(500).json({
+      errorMessage: e.message,
+    });
+  }
 }
--- route/ProductRouter.js
+++ route/ProductRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { saveProduct, updateProduct } from '../controller/ProductController.js';
+import { deleteProduct, saveProduct, updateProduct } from '../controller/ProductController.js';
 
 const router=express.Router();
 
 router.post('/saveProduct', saveProduct);
 router.put('/updateProduct/:id', updateProduct);
+router.delete('/deleteProduct/:id', deleteProduct);
 
 
 export default router;
