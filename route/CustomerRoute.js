ce6172be5935cd00862e76944457b373b0d4528e
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-17 23:47:40 +0530

    added role base authentication

--- route/CustomerRoute.js
+++ route/CustomerRoute.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import { deleteCustomer, getAllCustomers, getCustomerById, saveCustomer, updateCustomer } from '../controller/CustomerController.js';
+import { verify } from 'jsonwebtoken';
+import verifyToken from '../middleware/Auth.js';
 
 const router=express.Router();
 
-router.post('/saveCustomer', saveCustomer);
-router.put('/updateCustomer/:id',updateCustomer);
-router.delete('/deleteCustomer/:id', deleteCustomer);
-router.get('/getCustomerById/:id',getCustomerById);
-router.get('getAllCustomers', getAllCustomers);
+
+router.post('/saveCustomer',verifyToken(['admin','manager']), saveCustomer);
+router.put('/updateCustomer/:id',verifyToken(['admin']),updateCustomer);
+router.delete('/deleteCustomer/:id',verifyToken(['admin']), deleteCustomer);
+router.get('/getCustomerById/:id',verifyToken(['admin','manager','user']),getCustomerById);
+router.get('getAllCustomers',verifyToken(['admin','manager','user']), getAllCustomers);
 
 
 export default router;
