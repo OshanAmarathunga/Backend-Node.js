38b9b541c0ed7a08b34ac4c08d9efc5ea37d2e73
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-17 18:34:53 +0530

    added router

--- route/UserRoute.js
+++ route/UserRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controller/UserController.js';
+import { login, signup } from '../controller/UserController.js';
 
 const router=express.Router();
 
-router.post('signUpUser', signup);
+router.post('/signUpUser', signup);
+router.post('/login',login);
 
 export default router;
