88c53c6c4cf97ce2353c7b92a96f27279f82f588
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-17 18:15:55 +0530

    added signup

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,1 +1,38 @@
-import User from '../model/UserSchema.js'
+import User from "../model/UserSchema.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export async function signup(req, res) {
+  const { username, password, fullName, role } = req.body;
+  const userExist = await User.findOne({ username: username });
+
+  if (userExist) {
+    return res.status(400).json({
+      message: "Username is already exist",
+    });
+  }
+    const salt=await bcryptjs.genSalt(10);
+    const hashedPassword= bcryptjs.hash(password,salt);
+    const newUser= new User({
+        username,
+        password:hashedPassword,
+        fullName,
+        role,
+        isActive:true
+    });
+
+    const savedUser=await newUser.save();
+
+    res.status(200).json({
+        message:"User signup complete",
+        data:savedUser
+    })
+
+  try {
+  } catch (e) {
+    res.status(500).json({
+      errorMessage: e.message,
+    });
+  }
+}
