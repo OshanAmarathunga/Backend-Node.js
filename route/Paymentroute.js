c2db827b4de7a446216b3ae262ee053b8d65ca6a
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-20 19:53:24 +0530

    added payment verification

--- route/Paymentroute.js
+++ route/Paymentroute.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { findIncomeMonth, findIncomeToday, makePayment } from '../controller/PaymentController.js';
+import verifyToken from '../middleware/Auth.js';
 
 const router =express.Router();
 
-router.post('savePayment',makePayment);
-router.get('incomeByDay',findIncomeToday);
-router.get('incomeByMonth',findIncomeMonth);
-
+router.post('savePayment',verifyToken(['admin','manager']),makePayment);
+router.get('incomeByDay',verifyToken(['admin','manager']),findIncomeToday);
+router.get('incomeByMonth',verifyToken(['admin','manager']),findIncomeMonth);
 
 
 export default router;
